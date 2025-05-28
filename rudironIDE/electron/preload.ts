36cc203701dc243f321b82f296ce9f1e9ac6a390
ret7020 <ret7020@gmail.com>
2025-05-28 21:18:48 +0300

    Maybe works

--- rudironIDE/electron/preload.ts
+++ rudironIDE/electron/preload.ts
@@ -26,8 +26,93 @@ contextBridge.exposeInMainWorld('electron', {
         },
         showInputDialog: (options: { title: string; defaultValue: string }) =>
             ipcRenderer.invoke('show-input-dialog', options),
+        getSerialDevices: async (message) => {
+            return await ipcRenderer.invoke("request-serial-devices", message);
+        },
+        connectSerialDevice: async (message) => {
+            return await ipcRenderer.invoke("connect-serial-device", message);
+        },
+        writeSerial: (data) => ipcRenderer.invoke('send-serial', data),
+
+
+        // genPinMode: (pin, mode) => {
+        //     let bufferRes = Buffer.concat([
+        //         Buffer.from([0xFE, 0xDE, 0x1F, 0x00, 0xF4, 0x01, 0x02]),
+        //         genNumberArgument(pin),
+        //         genNumberArgument(mode)
+        //     ])
+        //     printBuffer(bufferRes);
+
+        //     return bufferRes;
+
+        // },
+
+        // genAnalogWrite: (pin, value) => {
+        //     let bufferRes = Buffer.concat([
+        //         Buffer.from([0xFE, 0xDE, 0x1F, 0x00, 0xF6, 0x01, 0x02]),
+        //         genNumberArgument(pin),
+        //         genNumberArgument(value)
+        //     ])
+
+        //     return bufferRes;
+        // }
     },
 });
+
+contextBridge.exposeInMainWorld('api', {
+    generators: {
+        pinMode: (pin, mode) => {
+            let bufferRes = Buffer.concat([
+                Buffer.from([0xFE, 0xDE, 0x1F, 0x00, 0xF4, 0x01, 0x02]),
+                genNumberArgument(pin),
+                genNumberArgument(mode)
+            ])
+
+            printBuffer(bufferRes);
+
+            return bufferRes;
+
+        },
+
+        analogWrite: (pin, value) => {
+            let bufferRes = Buffer.concat([
+                Buffer.from([0xFE, 0xDE, 0x1F, 0x00, 0xF6, 0x01, 0x02]),
+                genNumberArgument(pin),
+                genNumberArgument(value)
+            ])
+
+            return bufferRes;
+        },
+
+        ditialWrite: (pin, value) => {
+            let bufferRes = Buffer.concat([
+                Buffer.from([0xFE, 0xDE, 0x1F, 0x00, 0xF5, 0x01, 0x02]),
+                genNumberArgument(pin),
+                genNumberArgument(value)
+            ])
+
+            return bufferRes;
+        },
+
+        delay: (time) => {
+            let bufferRes = Buffer.concat([
+                Buffer.from([0xFE, 0xDE, 0x11, 0x00, 0x90, 0x01, 0x01]),
+                genNumberArgument(time)
+            ])
+
+            return bufferRes;
+
+        }
+
+    }
+})
+
+contextBridge.exposeInMainWorld('visualization_api', {
+    setDigitalPin: (callback) => ipcRenderer.on('board_visualization_digital', callback),
+    setAnalogPin: (callback) => ipcRenderer.on('board_visualization_analog', callback)
+})
+
+
 
 declare global {
     interface Window {
@@ -40,6 +125,7 @@ declare global {
                 send(channel: string, ...args: any[]): void;
                 invoke(channel: string, ...args: any[]): Promise<any>;
                 showInputDialog: (options: { title: string; defaultValue: string }) => Promise<string | undefined>;
+                getSerialDevices(message: string): void;
             };
         };
     }
